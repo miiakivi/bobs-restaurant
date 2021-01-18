8db5f212a3a17ef3a82d3ba5c2dd2eba9919164c
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2021-01-18 21:08:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 let content = document.getElementById('content');
 
-(function createHeader() {
+function createHeader() {
     let header = document.createElement('header');
     header.innerHTML = '<header><div class="head-filter"><div class="head-txt"><h1>Bob\'s Bistro & Bar</h1><p>Craftbeer, wine, burgers and breakfast</p><p>since 2012</p></div></div></header>';
     content.appendChild(header);
     console.log('hello');
-})()
+}
+
+createHeader();
